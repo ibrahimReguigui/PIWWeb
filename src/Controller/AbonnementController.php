282b82a7062c8c1ee6a108ef88fc136e8c5f3c1c
Sukuna13 <ahmedaziz.ghadhab@esprit.tn>
2022-04-14 12:18:44 +0100

    //

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/salle/abonnement")
  */
-class AbonnementController extends AbstractController
+class AbonnementController extends AbstractController  //
 {
     /**
      * @Route("/", name="app_abonnement_index", methods={"GET"})
